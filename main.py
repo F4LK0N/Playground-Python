0f95ed47069a1e47012c3d4b87e5512d3d746a5c
F4LK0N <otaviosoria@gmail.com>
2024-05-11 01:11:06 -0300

    Variables

--- main.py
+++ main.py
@@ -14,6 +14,25 @@ y = "Text"
 print(y)
 print(type(y))
 
+print("## Many Values to Multiple Variables ##")
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+print("## One Value to Multiple Variables ##")
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+print("## Unpack a Collection ##")
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
 
 
 ### CASTING ###
