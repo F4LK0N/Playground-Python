4b66174a2e90ac14d017852dbdeb243f83f7830e
F4LK0N <otaviosoria@gmail.com>
2024-05-11 02:35:18 -0300

    Conditionals - While

--- main.py
+++ main.py
@@ -252,6 +252,21 @@ if b > a:
     pass
 
 
+print("## WHILE ##")
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+
+
 
 ### ERRORS ###
 print("### ERRORS ###")
