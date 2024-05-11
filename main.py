72b5fdfa1c703d24b04526c005a39a4b7508d4db
F4LK0N <otaviosoria@gmail.com>
2024-05-11 03:22:52 -0300

    Arrays

--- main.py
+++ main.py
@@ -184,6 +184,32 @@ txt = "We are the so-called \"Vikings\" from the north."
 print(txt)
 
 
+
+### ARRAYS ###
+print("### ARRAYS ###")
+cars = ["Ford", "Volvo", "BMW"]
+print(cars[0])
+print(len(cars))
+for x in cars:
+    print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda")
+for x in cars:
+    print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.pop(1)
+for x in cars:
+    print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.remove("Volvo")
+for x in cars:
+    print(x)
+
+
+
 ### CONDITIONALS ###
 print("### CONDITIONALS ###")
 
