e1f4be0ac952bfe17d14e4ee65d71e46e25e0ca3
F4LK0N <otaviosoria@gmail.com>
2024-05-11 02:22:26 -0300

    Strings

--- main.py
+++ main.py
@@ -84,6 +84,106 @@ print(type(z))
 
 
 
+### STRINGS ###
+print("### STRINGS ###")
+print("Hello")
+print('Hello')
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+print("## Arrays ##")
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+
+print("## Check ##")
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
+print("## Slicing ##")
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+print("## Modify ##")
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(","))
+
+
+print("## Concatenation ##")
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+print("## Format ##")
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+
+print("## Escape ##")
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+
 ### CONDITIONALS ###
 print("### CONDITIONALS ###")
 
