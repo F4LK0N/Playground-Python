4243c04b48fca15cee469f2ea00a3296c71bc06a
F4LK0N <otaviosoria@gmail.com>
2024-05-11 00:37:54 -0300

    Variables

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-print("asd")
+### OUTPUTS ###
+print("### OUTPUTS ###")
+print("Hello World")
 
+
+
+### VARIABLES ###
+print("### VARIABLES ###")
 x = 1
 print(x)
 print(type(x))
@@ -7,3 +13,27 @@ print(type(x))
 y = "Text"
 print(y)
 print(type(y))
+
+
+
+### CONDITIONALS ###
+print("### CONDITIONALS ###")
+
+
+
+### ERRORS ###
+print("### ERRORS ###")
+
+
+
+### FUNCTIONS ###
+print("### FUNCTIONS ###")
+
+
+
+### CLASSES ###
+print("### CLASSES ###")
+
+
+
+
